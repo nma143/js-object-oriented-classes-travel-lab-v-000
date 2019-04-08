26e5f78471622d274c04c2c3d3bf46632661b88f
Nikki A <nikki.apostolakis@gmail.com>
2019-04-08 22:18:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ class Route
       'Madison Avenue',
       '5th Avenue'
     ];
-
+    Console.log("Hey");
     let numAvenues = this.endingLocation.horizontal - this.beginningLocation.horizontal;
     Console.log(numAvenues);
     let numStreets = eastWest.findIndex(this.endingLocation.vertical) - eastWest.findIndex(this.beginningLocation.vertical);
