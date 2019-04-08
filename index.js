4b97df4d56abfc362dd0e4f157d7e23d415e98e0
Nikki A <nikki.apostolakis@gmail.com>
2019-04-08 22:31:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+class Driver
+{
+  constructor(name, startDate)
+  {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year)
+  {
+      return year - this.startDate.getFullYear();
+  }
+}
+
+class Route
+{
+  constructor(beginningLocation, endingLocation)
+  {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled()
+  {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    let numVerticalBlocks = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    let numHorizontalBlocks = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+    return numVerticalBlocks + numHorizontalBlocks;
+  }
+
+  estimatedTime(peak)
+  {
+    if (peak)
+    {
+      return this.blocksTravelled()/2;
+    }
+    else
+    {
+      return this.blocksTravelled()/3;
+    }
+  }
+
+
+}
