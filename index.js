5273930bdab8b048f5a5e2d4d60a6ba224976a9b
Nikki A <nikki.apostolakis@gmail.com>
2019-04-08 22:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,11 +41,11 @@ class Route
   {
     if (peak)
     {
-      return this.blocksTravelled()/3;
+      return this.blocksTravelled()/2;
     }
     else
     {
-      return this.blocksTravelled()/2;
+      return this.blocksTravelled()/3;
     }
   }
 
