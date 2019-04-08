215d12ab145dd829cb4450d379ceec1b17596871
Nikki A <nikki.apostolakis@gmail.com>
2019-04-08 22:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,21 @@ class Route
 
   blocksTravelled()
   {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    let numAvenues = endingLocation.horizontal - beginningLocation.horizontal;
     
+
+
+
+
   }
 }
